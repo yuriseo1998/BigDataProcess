ff2ee19766854f65978701530539acf137addf00
bigdata <yuri_seo1998@naver.com>
2022-11-01 22:28:33 +0900

    final

--- HW3/UBERStudent20191768.py
+++ HW3/UBERStudent20191768.py
@@ -10,5 +10,19 @@ result = {}
 with open(sys.argv[1]) as file:
     for line in file:
     	data = line.strip().split(',')
-    	print("{},{} {},{}".format(data[0], get_days(data[1]), data[2], data[3]))
+    	data[1] = get_days(data[1])
+    	data[2] = int(data[2])
+    	data[3] = int(data[3])
+    	if len(result) == 0:
+    		result[(data[0], data[1])] = [data[2],data[3]]
+    	else:
+    		if (data[0], data[1]) in result.keys():
+    			temp = result[(data[0], data[1])]
+    			temp[0] += data[2]
+    			temp[1] += data[3]
+    			result[(data[0], data[1])]= temp
+    		else:
+    			result[(data[0], data[1])] = [data[2],data[3]]
+for k, v in result.items():
+	print("{},{} {},{}".format(k[0], k[1], v[0], v[1]))
 sys.stdout.close()
